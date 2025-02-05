316a8abbc6bf47934364b3c4bdd700d17af1ef3f
sebastienDOC <s.dechand21@gmail.com>
2025-02-05 11:39:11 +0100

    spring7: Many-to-Many sans attribut

--- src/main/java/org/wildcodeschool/myblog/dto/ArticleDTO.java
+++ src/main/java/org/wildcodeschool/myblog/dto/ArticleDTO.java
@@ -1,6 +1,7 @@
 package org.wildcodeschool.myblog.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ArticleDTO {
     private Long id;
@@ -8,6 +9,7 @@ public class ArticleDTO {
     private String content;
     private LocalDateTime updatedAt;
     private String categoryName;
+    private List<String> imageUrls;
 
     // Getters et setters
 
@@ -50,5 +52,13 @@ public class ArticleDTO {
 
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
+    }
+
+    public List<String> getImageUrls() {
+        return imageUrls;
+    }
+
+    public void setImageUrls(List<String> imageUrls) {
+        this.imageUrls = imageUrls;
     }
 }
--- src/main/java/org/wildcodeschool/myblog/model/Article.java
+++ src/main/java/org/wildcodeschool/myblog/model/Article.java
@@ -2,6 +2,7 @@ package org.wildcodeschool.myblog.model;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 public class Article {
@@ -25,6 +26,14 @@ public class Article {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @ManyToMany
+    @JoinTable(
+            name = "article_image",
+            joinColumns = @JoinColumn(name = "article_id"),
+            inverseJoinColumns = @JoinColumn(name = "image_id")
+    )
+    private List<Image> images;
 
     // Getters et setters
 
@@ -74,5 +83,13 @@ public class Article {
 
     public void setCategory(Category category) {
         this.category = category;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
     }
 }
